253fc936e67a5c2fa30b69a3edad03122e6001f8
Joaquin Fox <joaquin.fox@gmail.com>
2019-01-29 13:14:49 -0700

    cleanup more items related to protected data

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import requiresLogin from './requires-login';
-import { fetchProtectedData } from '../actions/protected-data';
 import Card from './card';
 
 export class Dashboard extends React.Component {
