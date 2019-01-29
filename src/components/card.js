c52a734531921f2123319defa72ae734e44f89cb
Joaquin Fox <joaquin.fox@gmail.com>
2019-01-29 11:56:53 -0700

    abandoned branch for rendering question

--- src/components/card.js
+++ src/components/card.js
@@ -1,14 +1,33 @@
 import React from 'react';
+// this will be my component to render a question and collect the user response.
+// It needs to be stateful on the first implementation.
+export default class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: [
+        { question1: 'answer1' },
+        { question2: 'answer2' },
+        { question3: 'answer3' }
+      ]
+    };
+  }
 
-export function Card(props) {
-
-  return(
-    <div className="card-wrapper">
-      <ul className="card-list">
-        <li className="cards">
-          <span>Level 1</span>
-        </li>
-      </ul>
-    </div>
-  )
+  render() {
+    // let keys = Object.keys(this.state.questions);
+    const questionBox = 'questionBox';
+    const answerField = 'answerField';
+    return (
+      <div className="card-wrapper">
+        <fieldset>
+          <legend>Tagalog</legend>
+          <form action="submit">
+            <label for="answer-submit" />
+            <input type="text" name="answer-submit" />
+            <div className="response" />
+          </form>
+        </fieldset>
+      </div>
+    );
+  }
 }
--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import requiresLogin from './requires-login';
 import {fetchProtectedData} from '../actions/protected-data';
-// import Card from './card';
+import Card from './card';
 
 export class Dashboard extends React.Component {
     componentDidMount() {
@@ -14,14 +14,14 @@ export class Dashboard extends React.Component {
         return (
             <div className="dashboard">
                 <div className="dashboard-username">
-                    Username: {this.props.username}
+                    {/* Username: {this.props.username} */}
                     <h2>Hello {this.props.name}!</h2>
                 </div>
                 <div className="dashboard-name">Name: {`${this.props.name}`}</div>
                 <div className="dashboard-protected-data">
                     Protected data: {this.props.protectedData}
                 </div>
-                {/* <Card /> */}
+                <Card />
             </div>
         );
     }
