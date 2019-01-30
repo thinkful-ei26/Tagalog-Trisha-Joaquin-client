0eee02b4706cf6a663ef62836a7e21c3ea8795a9
kronicle114 <trisha.aguinaldo@gmail.com>
2019-01-30 12:31:16 -0500

    deleted useless branch

--- src/components/answer-feedback-correct.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function AnswerFeedbackCorrect(props) {
-  return (
-    <div className="answer-feedback">
-      <div className="answer-feedback-correct">Correct!</div>
-    </div>
-  );
-}
--- src/components/answer-feedback-incorrect.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function AnswerFeedbackInorrect(props) {
-  return (
-    <div className="answer-feedback">
-      <div className="answer-feedback-wrong">Incorrect</div>
-    </div>
-  );
-}
