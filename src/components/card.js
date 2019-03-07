61dc76583472003fa7b5ca9514fdfa12c5ecae93
Trisha Aguinaldo <trisha.aguinaldo@gmail.com>
2019-03-07 12:17:22 -0500

    deleted unused reset & reset css

--- src/components/card.js
+++ src/components/card.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { connect, reset } from 'react-redux';
+import { connect } from 'react-redux';
 import { reduxForm, Field } from 'redux-form';
 import { required, nonEmpty } from '../validators';
 import { putAnswer, updateCorrect } from '../actions/question';
